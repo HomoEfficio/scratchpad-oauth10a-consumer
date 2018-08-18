3b4232cca917b451f4c82b3363193439c0e60646
Homo Efficio <homo.efficio@gmail.com>
2018-08-19 01:46:41 +0900

    OAuth 1.0a 스펙대로 Signature 구현

--- src/main/java/io/homo/efficio/scratchpad/oauth10a/consumer/controller/MentionController.java
+++ src/main/java/io/homo/efficio/scratchpad/oauth10a/consumer/controller/MentionController.java
@@ -1,12 +1,18 @@
 package io.homo.efficio.scratchpad.oauth10a.consumer.controller;
 
 import io.homo.efficio.scratchpad.oauth10a.consumer.domain.Mention;
+import io.homo.efficio.scratchpad.oauth10a.consumer.domain.TemporaryCredentialsRequestHeader;
+import io.homo.efficio.scratchpad.oauth10a.consumer.util.OAuthUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author homo.efficio@gmail.com
@@ -17,6 +23,12 @@ import org.springframework.web.servlet.ModelAndView;
 @Slf4j
 public class MentionController {
 
+    @Value("${oauth10a.consumer.key}")
+    private String consumerKey;
+
+    @Value("${oauth10a.consumer.secret}")
+    private String consumerSecret;
+
     @GetMapping
     public ModelAndView showMentionForm(ModelAndView mav) {
         mav.setViewName("mentionForm");
@@ -24,8 +36,13 @@ public class MentionController {
     }
 
     @PostMapping
-    public String writeToTwitter(Mention mention) {
+    @ResponseBody
+    public String writeToTwitter(HttpServletRequest request, Mention mention) {
+        final TemporaryCredentialsRequestHeader tcHeader
+                = new TemporaryCredentialsRequestHeader(request, this.consumerKey);
+        String signature = OAuthUtils.generateSignature(tcHeader, this.consumerSecret, "");
         log.info("### mention: " + mention.getMention());
+        log.info("### signature: " + signature);
         return "redirect:" + "https://www.daum.net";
     }
 }
---
+++ src/main/java/io/homo/efficio/scratchpad/oauth10a/consumer/util/OAuthConstants.java
@@ -0,0 +1,30 @@
+package io.homo.efficio.scratchpad.oauth10a.consumer.util;
+
+/**
+ * @author homo.efficio@gmail.com
+ * Created on 2018-08-18.
+ */
+public interface OAuthConstants {
+
+    String HTTP_METHOD_POST = "POST";
+
+    String OAUTH_CONSUMER_KEY = "oauth_consumer_key";
+
+    String OAUTH_NONCE = "oauth_nonce";
+
+    String OAUTH_SIGNATURE = "oauth_signature";
+
+    String OAUTH_SIGNATURE_METHOD = "oauth_signature_method";
+
+    String OAUTH_TIMESTAMP = "oauth_timestamp";
+
+    String OAUTH_TOKEN = "oauth_token";
+
+    String OAUTH_VERSION = "oauth_version";
+
+    String VERSION_1_0 = "1.0";
+
+    String OAUTH_SIGNATURE_METHOD_HMAC_SHA1 = "HMAC_SHA1";
+
+    String HMAC_SHA1_ALGORITHM_NAME = "HmacSHA1";
+}
